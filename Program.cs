782fbbb799bb87dcf75a697b04f9af40b3a2fbcc
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 22:33:18 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if(numero >= 0)
+{ 
+    Console.ForegroundColor = ConsoleColor.Green; 
+}
+else 
+{
+      Console.ForegroundColor = ConsoleColor.Red;
+}
+Console.WriteLine($"o número digitado é {numero}.");
+
+Console.ResetColor();
